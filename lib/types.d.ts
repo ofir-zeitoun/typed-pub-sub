ee4ce2452adf00e4aef1d99695c80cb721d4c0ff
Ofir Zeitoun <ofir.zeitoun@MB-C02GC20SML85.brightmachines.com>
2022-05-31 14:03:38 +0300

    rename Key

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -1,7 +1,7 @@
 export declare type PubSubCreator<T> = () => {
-    [KeyOn in keyof T as `on${Capitalize<string & KeyOn>}`]: (callback: (payload: T[KeyOn]) => void) => Function;
+    [Key in keyof T as `on${Capitalize<string & Key>}`]: (callback: (payload: T[Key]) => void) => Function;
 } & {
-    [KeyOn in keyof T as `once${Capitalize<string & KeyOn>}`]: (callback: (payload: T[KeyOn]) => void) => Function;
+    [Key in keyof T as `once${Capitalize<string & Key>}`]: (callback: (payload: T[Key]) => void) => Function;
 } & {
     [KeySet in keyof T as `set${Capitalize<string & KeySet>}`]: (payload: T[KeySet]) => void;
 };
